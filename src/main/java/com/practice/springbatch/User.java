e12dcb6d0b59a308f477eb6c528b205de9cdfeff
kyoungbeom <pkb1998@naver.com>
2024-12-14 22:05:33 +0900

    FlatFileItemReader 구현 - delimiter 기반

---
+++ src/main/java/com/practice/springbatch/User.java
@@ -0,0 +1,15 @@
+package com.practice.springbatch;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+
+    private String age;
+
+    private String region;
+
+    private String telephone;
+}
